072d3b6062dd8163b5cd8b6661021c4d2a581b8b
martyu <marty@martyulrich.com>
2013-10-26 20:10:26 -0700

    Initial Commit

---
+++ CS450_Assignment2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CS450_Assignment2
+//
+//  Created by Marty on 10/26/13.
+//  Copyright (c) 2013 Marty. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
